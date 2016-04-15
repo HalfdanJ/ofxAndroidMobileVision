40e6e0b2c3487b012d60758971c7ad577b506fa3
Jonas Jongejan <jonas@halfdanj.dk>
2016-04-15 14:37:24 +0200

    readme

---
+++ README.mk
@@ -0,0 +1,8 @@
+# ofxAndroidMobileVision
+An addon that enables access to the [Google Mobile Vision API](https://developers.google.com/vision/).
+The addon is usefull for easy face tracking including sentiment analysis, besides barcode detecion.
+
+The addons works only on Android, as it accesses a native android library.
+
+# Requirements
+Works only on latest version of openFrameworks directly from git. 
